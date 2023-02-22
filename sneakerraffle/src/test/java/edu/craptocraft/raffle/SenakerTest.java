227e9ba0819e70e2d341155b7ba3171070208c88
Amin Mustafa Boankod <amin.m.boankod@gmail.com>
2023-02-22 23:56:08 +0100

    refactorizar(getSizes): Refactorizar y añadir más casos test a getSizes

--- sneakerraffle/src/test/java/edu/craptocraft/raffle/SenakerTest.java
+++ sneakerraffle/src/test/java/edu/craptocraft/raffle/SenakerTest.java
@@ -2,10 +2,15 @@ package edu.craptocraft.raffle;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
+
+import java.util.EnumSet;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import edu.craptocraft.Sizes;
 
 
 public class SenakerTest {
@@ -13,11 +18,12 @@ public class SenakerTest {
     Raffle craft;
     Entry entry;
     Entry doubleEntry;
+    EnumSet<Sizes> sizes;
     
 
     @Before
     public void setup() {
-        doubleEntry = new Entry("squan.chy@closet.in");
+        doubleEntry = new Entry("squanchy@closet.in");
         entry = new Entry("squanchy@closet.in");
         craft = new Sneaker("Nike Craft General Purpose", "Brown", 109.99);
     }
@@ -28,14 +34,16 @@ public class SenakerTest {
     public void constructorTest(){
         assertNotNull(craft);
     }
+    
 
     @Test
     public void toStringTest(){
-        assertEquals("Nike Craft General Purpose\nBrown\n109.99 €\nnull", craft.toString());
+        craft.sizesRun(Sizes.CUARENTA, Sizes.CUARENTAYDOS);
+        assertEquals("Nike Craft General Purpose\nBrown\n109.99 €\n[7.0 US, 7.5 US, 8.0 US, 8.5 US]", craft.toString());
     }
 
     @Test
-    public void testRegister() {
+    public void registerTest() {
         assertEquals(0, craft.getRaffles().size());
         craft.register(entry);
         assertEquals(1, craft.getRaffles().size());
@@ -48,6 +56,13 @@ public class SenakerTest {
         assertEquals(1, craft.getRaffles().size());
 
     }
+
+    @Test
+    public void getSizesTest() {
+        craft.sizesRun(Sizes.TREINTAYNUEVE, Sizes.CUARENTA); 
+        assertEquals("Nike Craft General Purpose\nBrown\n109.99 €\n[6.5 US, 7.0 US]", craft.toString());
+    }
+
 
 
 
